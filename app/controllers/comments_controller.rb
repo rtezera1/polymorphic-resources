b7f340f717c70437f076aeb7865366a34fb801a6
Robel <robeltadesse01@gmail.com>
2014-05-21 15:34:02 -0400

    fixed bug

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -9,7 +9,7 @@ class CommentsController < ApplicationController
   end
 
   def create
-    @comment = @commentable.comments.new(params[:comment])
+    @comment = @commentable.comments.new(comment_params)
     if @comment.save
       redirect_to @commentable, notice: 'Comment created.'
     else
@@ -22,5 +22,9 @@ class CommentsController < ApplicationController
   def load_commentable
     resource, id = request.path.split('/') [1,2]
     @commentable = resource.singularize.classify.constantize.find(id)
+  end
+
+  def comment_params
+    params.require(:comment).permit(:content)
   end
 end
